48228f55da56dbb9af1c7245364243f490b0deba
Panosthelion <34085551+Panosthelion@users.noreply.github.com>
2018-04-23 10:06:33 +0300

    Update FXMLDocumentController.java

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculatorfx;
 
 import java.net.URL;
@@ -14,10 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-/**
- *
- * @author Villan
- */
+
 public class FXMLDocumentController implements Initializable {
 
     Float data = 0f;
